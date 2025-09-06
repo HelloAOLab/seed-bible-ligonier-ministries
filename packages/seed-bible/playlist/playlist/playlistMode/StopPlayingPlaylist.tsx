1592a06d46ecd958629869969a6412a364cdac8c
kushagra <hykukku@gmail.com>
2025-09-06 19:40:29 +0530

    Added Latest Playlist Code to github

---
+++ packages/seed-bible/playlist/playlist/playlistMode/StopPlayingPlaylist.tsx
@@ -0,0 +1,12 @@
+const parentId = that?.parentId || 'default';
+
+DataManager.cancelCurrentPlayingSound();
+globalThis.SetSelected && SetSelected({});
+globalThis.SetHolded && SetHolded({});
+// globalThis.SetPlayingPlaylist && globalThis.SetPlayingPlaylist(false);
+globalThis[`${parentId}ToggleGreyCheckPLayingPlaylist`] && globalThis[`${parentId}ToggleGreyCheckPLayingPlaylist`](null);
+globalThis.IsQueuePresent = false;
+// os.unregisterApp("playing-playlist");
+globalThis.IS_PLAYLIST_ACTIVE = false;
+globalThis.SetSplitAppPanel2(null);
+globalThis.StopPlayingPlaylistModal(false);
