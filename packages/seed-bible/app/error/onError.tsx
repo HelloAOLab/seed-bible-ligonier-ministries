df052fd284a47af4dd2bbc3095bd42244bc35c60
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-09-29 12:04:37 -0400

    Save the pattern and pattern version when reporting errors

--- packages/seed-bible/app/error/onError.tsx
+++ packages/seed-bible/app/error/onError.tsx
@@ -4,6 +4,12 @@ try {
         bot: that.error.bot?.id,
         tag: that.error.tag,
         system: that.error.bot?.tags.system,
+        pattern: configBot.tags.pattern,
+        patternVersion: configBot.tags.pattern ? getBot(
+            byTag('aoArtifact', true),
+            byTag('origin_ao', configBot.tags.pattern),
+            byTag('origin_version')
+        )?.tags?.origin_version : undefined,
     });
 } catch(err) {
     console.error('Error reporting the error to PostHog:', err);
