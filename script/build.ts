b1c17ced136983882985f6b0950666d847036855
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-09-29 11:14:57 -0400

    Support building every file in the lib folder

--- script/build.ts
+++ script/build.ts
@@ -1,15 +1,18 @@
 import * as esbuild from 'esbuild';
 import { existsSync } from 'node:fs';
-import { rm } from 'node:fs/promises';
+import { readdir, rm } from 'node:fs/promises';
+import path from 'node:path';
 
 if (existsSync('lib/dist')) {
     await rm('lib/dist', { recursive: true });
 }
 
+const entryPoints = (await readdir('lib', { withFileTypes: true }))
+    .filter(dirent => dirent.isFile())
+    .map(file => path.join(file.parentPath, file.name));
+
 await esbuild.build({
-    entryPoints: [
-        'lib/vendor.ts'
-    ],
+    entryPoints,
     assetNames: 'assets/[name]-[hash]',
     entryNames: '[name]-[hash]',
     bundle: true,
