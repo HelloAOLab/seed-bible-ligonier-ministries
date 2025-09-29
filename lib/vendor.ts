2f02b507555c62d2441730ba67e3d0589b57e3ed
Kallyn Gowdy <kallyngowdy@gmail.com>
2025-09-29 10:53:36 -0400

    Export everything from @tiptap/extension-text-style

--- lib/vendor.ts
+++ lib/vendor.ts
@@ -1,12 +1,10 @@
 export * from '@tiptap/core';
 import StarterKit from "@tiptap/starter-kit";
-import { Color } from "@tiptap/extension-color";
 import TextAlign from "@tiptap/extension-text-align";
 import Underline from "@tiptap/extension-underline";
 import Superscript from "@tiptap/extension-superscript";
 import Subscript from "@tiptap/extension-subscript";
 import Highlight from "@tiptap/extension-highlight";
-import { TextStyle } from "@tiptap/extension-text-style";
 import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import BulletList from "@tiptap/extension-bullet-list";
@@ -14,6 +12,7 @@ import OrderedList from "@tiptap/extension-ordered-list";
 import ListItem from "@tiptap/extension-list-item";
 import preactRenderToString from 'preact-render-to-string';
 
+export * from "@tiptap/extension-text-style";
 export {
     TextAlign,
     Underline,
