338a634ad160cb7d7a72b9828ff6a68bf9f4062b
kushagra <hykukku@gmail.com>
2025-09-30 19:02:35 +0530

    Changes how queue button works in different case

--- packages/Playlist/playlist/playlistMode/CloseSelf.tsx
+++ packages/Playlist/playlist/playlistMode/CloseSelf.tsx
@@ -1,22 +1,22 @@
-const isMobile = true || gridPortalBot.tags.pixelWidth < MOBILE_VIEWPORT_THRESHOLD;
+const isMobile = gridPortalBot.tags.pixelWidth < MOBILE_VIEWPORT_THRESHOLD;
 
 if (isMobile && globalThis.makingPlaylist) {
-    if (globalThis["Playlist_package"]) {
-        globalThis["Playlist_package"].onClick();
-    } else {
-        globalThis.isRecording = false;
-        globalThis.SelectedItemIDForAttachments = null;
-        Playlist.RemoveScreenRecordingControls();
-        try {
-            await experiment.endRecording();
-        } catch (err) { }
-        globalThis.StopVideoRecording = false;
-        RemoveApplicationByID(globalThis.PLAYLIST_PANEL_ID);
-        globalThis.PLAYLIST_PANEL_ID = null;
-        globalThis[`defaultToggleGreyCheckPLayingPlaylist`] &&
-            globalThis[`defaultToggleGreyCheckPLayingPlaylist`](null);
-        globalThis.IS_PLAYLIST_ACTIVE = false;
-        globalThis.SetSplitAppPanel2(null);
-        globalThis.makingPlaylist = false;
-    }
+  if (globalThis["Playlist_package"]) {
+    globalThis["Playlist_package"].onClick();
+  } else {
+    globalThis.isRecording = false;
+    globalThis.SelectedItemIDForAttachments = null;
+    Playlist.RemoveScreenRecordingControls();
+    try {
+      await experiment.endRecording();
+    } catch (err) {}
+    globalThis.StopVideoRecording = false;
+    RemoveApplicationByID(globalThis.PLAYLIST_PANEL_ID);
+    globalThis.PLAYLIST_PANEL_ID = null;
+    globalThis[`defaultToggleGreyCheckPLayingPlaylist`] &&
+      globalThis[`defaultToggleGreyCheckPLayingPlaylist`](null);
+    globalThis.IS_PLAYLIST_ACTIVE = false;
+    globalThis.SetSplitAppPanel2(null);
+    globalThis.makingPlaylist = false;
+  }
 }
