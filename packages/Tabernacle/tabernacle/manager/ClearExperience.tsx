a95835d54b3128e4af920764583cb3e46e355281
Gabriel Souki Hernandez <gabrielsouki@gmail.com>
2025-10-04 17:57:18 -0400

    Fixed that blueprint piece were not hidden when the experience was cleared

--- packages/Tabernacle/tabernacle/manager/ClearExperience.tsx
+++ packages/Tabernacle/tabernacle/manager/ClearExperience.tsx
@@ -1,24 +1,2 @@
-const keys = [
-    "altar-of-sacrifice",
-    "ark-of-covenant",
-    "bronze-laver",
-    "incense-altar",
-    "menorah",
-    "table-showbread",
-    "cloth-brown",
-    "cloth-red",
-    "cloth-grey",
-    "cloth-purple",
-    "ground",
-    "fence",
-    "structure-front-pillars",
-    "structure-inner-pillars",
-    "structure-walls",
-    "structure-rings",
-    "structure-bars",
-    "structure-inner-curtain",
-    "structure-front-curtain",
-]
-
 // Development purposes
-thisBot.SetBotsVisibility({data: keys.map((key) => { return {key, value: MeshState.Hidden} })});
+thisBot.SetBotsVisibility({data: thisBot.tags.piecesKeys.map((key) => { return {key, value: MeshState.Hidden} })});
