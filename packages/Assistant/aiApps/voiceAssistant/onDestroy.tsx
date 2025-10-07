f2d4f2e897151f4ef5ab1f57c56d637dc4f1444b
Bored-Wizard <sujanbarman12345dq@gmail,com>
2025-10-07 18:10:56 +0530

    need to move the sleep

--- packages/Assistant/aiApps/voiceAssistant/onDestroy.tsx
+++ packages/Assistant/aiApps/voiceAssistant/onDestroy.tsx
@@ -2,8 +2,8 @@ let prevPainter = document.getElementById("voiceAssistant-container");
 
 if (prevPainter) {
     if(globalThis?.AISetStart){
-        AISetStart(false);
         await os.sleep(5000);
+        AISetStart(false);
     }
     prevPainter.remove();
 }
