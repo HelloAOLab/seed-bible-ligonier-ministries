c9d6607722055bee80ff144ed64b6ccd380e73eb
Gabriel Souki Hernandez <gabrielsouki@gmail.com>
2025-10-12 16:37:50 -0400

    Fixed some inconsistent behavior with activity notification when dragging and dropping chapters

--- packages/Scripture Map 3D/scriptureMap3D/main/TryHighlightChapter.tsx
+++ packages/Scripture Map 3D/scriptureMap3D/main/TryHighlightChapter.tsx
@@ -1,12 +1,19 @@
 const {chapterData} = that;
 
+console.log(`[Debug] TryHighlightChapter`, {
+    piece: JSON.parse(JSON.stringify(chapterData.piece)),
+    "chapterData.piece.masks.isOnTheGround": chapterData.piece.masks.isOnTheGround,
+    "!chapterData.isSelected": !chapterData.isSelected,
+    hiddingActivityNotificatioOnPiece: chapterData.piece.masks.isOnTheGround && !chapterData.isSelected
+});
+
 if(chapterData.piece.masks.isSelecting        || 
     chapterData.piece.masks.isDeselecting     ||
     chapterData.piece.masks.isBeingDragged     ||
     (chapterData.piece.masks.isHighlighted && !chapterData.piece.masks.isUnhighlighting)) return false;
 
-if(chapterData.piece.masks.isOnTheGround && !chapterData.isSelected) 
-{
+// if(chapterData.piece.masks.isOnTheGround && !chapterData.isSelected) 
+// {
     BibleVizUtils.Functions.TryHideActivityNotificationOnPiece({piece: chapterData.piece})
-}
+// }
 chapterData.piece.Highlight({chapterData});
