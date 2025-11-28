9ec4ecab52f250b9e796a6a6909d512cdede0258
Gabriel Souki Hernandez <gabrielsouki@gmail.com>
2025-11-28 17:42:43 -0400

    Added a shout that triggers when the user logs in to add the authBot id to the list of users in scripture map 2D. Return null in settings if there are no users to show when reading history is enabled

--- packages/Scripture Map 2D/scriptureMap2D/main/Settings.tsx
+++ packages/Scripture Map 2D/scriptureMap2D/main/Settings.tsx
@@ -1,27 +1,48 @@
 // import { UserPresenceSettings } from "scriptureMap2D.main.UserPresenceSettings"
-import { useScriptureMap2DContext } from "scriptureMap2D.main.ScriptureMap2DContext"
-import {ProjectFiltersSelector} from "scriptureMap2D.main.ProjectFiltersSelector"
-import { ProjectStateSetter } from "scriptureMap2D.main.ProjectStateSetter"
-import { ReadingHistoryUserFiltersSelector } from "scriptureMap2D.main.ReadingHistoryUserFiltersSelector"
-import { ReadingHistoryTimeline } from "scriptureMap2D.main.ReadingHistoryTimeline"
+import { useScriptureMap2DContext } from "scriptureMap2D.main.ScriptureMap2DContext";
+import { ProjectFiltersSelector } from "scriptureMap2D.main.ProjectFiltersSelector";
+import { ProjectStateSetter } from "scriptureMap2D.main.ProjectStateSetter";
+import { ReadingHistoryUserFiltersSelector } from "scriptureMap2D.main.ReadingHistoryUserFiltersSelector";
+import { ReadingHistoryTimeline } from "scriptureMap2D.main.ReadingHistoryTimeline";
+import { useReadingHistoryContext } from "scriptureMap2D.main.ReadingHistoryContext";
 
 export const Settings = () => {
-    const { isUserPresenceEnabled, mode, ScriptureMap2DModes, project, isInSelectionMode, isReadingHistoryEnabled } = useScriptureMap2DContext();
+  const {
+    isUserPresenceEnabled,
+    mode,
+    ScriptureMap2DModes,
+    project,
+    isInSelectionMode,
+    isReadingHistoryEnabled,
+  } = useScriptureMap2DContext();
+  const { usersAuthId } = useReadingHistoryContext();
 
-    if(mode === ScriptureMap2DModes.Project && !project) return null;
+  if (
+    (mode === ScriptureMap2DModes.Project && !project) ||
+    (mode === ScriptureMap2DModes.Viewer &&
+      isReadingHistoryEnabled &&
+      usersAuthId.length === 0)
+  )
+    return null;
 
-    return (
-        <div className="mapSettings">
-            {mode === ScriptureMap2DModes.Project && project && <>
-                <ProjectStateSetter />
-                {!isInSelectionMode && <ProjectFiltersSelector />}
-            </>}
-            {mode === ScriptureMap2DModes.Viewer && isReadingHistoryEnabled && <>
-                <ReadingHistoryUserFiltersSelector />
-                <ReadingHistoryTimeline />
-            </>}
-        </div>
-    )
-}
+  return (
+    <div className="mapSettings">
+      {mode === ScriptureMap2DModes.Project && project && (
+        <>
+          <ProjectStateSetter />
+          {!isInSelectionMode && <ProjectFiltersSelector />}
+        </>
+      )}
+      {mode === ScriptureMap2DModes.Viewer &&
+        isReadingHistoryEnabled &&
+        usersAuthId?.length > 0 && (
+          <>
+            <ReadingHistoryUserFiltersSelector />
+            <ReadingHistoryTimeline />
+          </>
+        )}
+    </div>
+  );
+};
 
 // {isUserPresenceEnabled && <UserPresenceSettings />}
