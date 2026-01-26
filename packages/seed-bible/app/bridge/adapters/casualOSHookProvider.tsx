831ceb966aa0a26b2484483086448f2aca3f8f6c
LandonSiler <LandonSiler@users.noreply.github.com>
2026-01-26 11:09:48 -0500

    fix: Remove seperate feature concern

--- packages/seed-bible/app/bridge/adapters/casualOSHookProvider.tsx
+++
@@ -1,6 +0,0 @@
-import type { HookProvider } from "app.contract.provider";
-
-/**
- * A hooks standard mapped to casualOS api's providing said functionality.
- */
-export const casualOSHookProvider: HookProvider = os.appHooks;
