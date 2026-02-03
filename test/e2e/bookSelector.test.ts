e4719a13fa044065a18d811d5061608ea3e35534
Bored-Wizard <sujanbarman12345dq@gmail,com>
2026-02-03 14:25:42 +0530

    U+1F605

--- test/e2e/bookSelector.test.ts
+++ test/e2e/bookSelector.test.ts
@@ -8,8 +8,7 @@ console.log = jest.fn();
 
 beforeAll(async () => {
   browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    headless: false,
+    args: ["--no-sandbox"],
   });
 });
 
