5e8b4278b5ad115d89796d55c89e079dcc9751ed
Amit Pant <amitpant@Amits-MacBook-Air.local>
2026-02-23 23:01:52 +0530

    bibleStack removed from preset=minimal

--- packages/seed-bible/app/components/thePage.tsx
+++ packages/seed-bible/app/components/thePage.tsx
@@ -134,7 +134,11 @@ function ThePage({
   const [bible, setBible] = useState();
   const [footnotes, setFootnotes] = useState(() => {
     if (tab) {
-      return getCachedFootnotes(tab.data?.translation, tab.data?.bookId, tab.data?.chapter);
+      return getCachedFootnotes(
+        tab.data?.translation,
+        tab.data?.bookId,
+        tab.data?.chapter
+      );
     }
     return null;
   });
@@ -427,13 +431,13 @@ function ThePage({
                 chapterNo = configBot.tags.chapter;
               const chapterUrl = chapterNo
                 ? bookData.firstChapterApiLink.replace(
-                  "1.json",
-                  `${chapterNo}.json`
-                )
+                    "1.json",
+                    `${chapterNo}.json`
+                  )
                 : bookData.firstChapterApiLink.replace(
-                  "1.json",
-                  `${tab.data.chapter}.json`
-                );
+                    "1.json",
+                    `${tab.data.chapter}.json`
+                  );
               await bible.open(
                 bookData.id,
                 configBot.tags.chapter || 1,
@@ -453,13 +457,13 @@ function ThePage({
               chapterNo = configBot.tags.chapter;
             const chapterUrl = chapterNo
               ? bookData.firstChapterApiLink.replace(
-                "1.json",
-                `${chapterNo}.json`
-              )
+                  "1.json",
+                  `${chapterNo}.json`
+                )
               : bookData.firstChapterApiLink.replace(
-                "1.json",
-                `${tab.data.chapter}.json`
-              );
+                  "1.json",
+                  `${tab.data.chapter}.json`
+                );
             await bible.open(
               bookData.id,
               configBot.tags.chapter || 1,
@@ -750,8 +754,9 @@ function ThePage({
       // Build context data
       setContextData({
         verse: selectedTextFinal,
-        reference: `${data?.book} ${data?.chapter}:${lowestVerse}${lowestVerse !== highestVerse ? `-${highestVerse}` : ""
-          }`,
+        reference: `${data?.book} ${data?.chapter}:${lowestVerse}${
+          lowestVerse !== highestVerse ? `-${highestVerse}` : ""
+        }`,
         book: data?.book,
         chapter: data?.chapter,
         verses: unifiedVerses,
@@ -954,8 +959,8 @@ function ThePage({
               createAttributes: config?.createAttributes
                 ? config.createAttributes
                 : () => {
-                  return {};
-                },
+                    return {};
+                  },
             };
           });
         });
@@ -1026,11 +1031,11 @@ function ThePage({
       openPrevChapter,
       open,
       changeTranslation: bible?.changeTranslation || undefined,
-      setPanalApp: () => { },
+      setPanalApp: () => {},
     });
     globalThis.Open = open;
     globalThis.ChangeTranslation = changeTranslation;
-    globalThis.SetPanalApp = () => { };
+    globalThis.SetPanalApp = () => {};
     globalThis.ToggleVerseHighlight = toggleVerseHighlight;
     globalThis.UnHighlightVerse = unHighlightVerse;
     globalThis.HighlightVerse = highlightVerse;
@@ -1182,10 +1187,11 @@ function ThePage({
     (verseNumbers, color, scroll, fadeIn, skipIt) => {
       if (!tab?.id) return;
 
-      const verseId = `v-${Array.isArray(verseNumbers)
-        ? verseNumbers[verseNumbers.length - 1]
-        : verseNumbers
-        }`;
+      const verseId = `v-${
+        Array.isArray(verseNumbers)
+          ? verseNumbers[verseNumbers.length - 1]
+          : verseNumbers
+      }`;
 
       if (scroll)
         document.getElementById(verseId)?.scrollIntoView({
@@ -1281,10 +1287,11 @@ function ThePage({
     (verseNumbers, color, scroll = true) => {
       if (!tab?.id) return;
 
-      const verseId = `v-${Array.isArray(verseNumbers)
-        ? verseNumbers[verseNumbers.length - 1]
-        : verseNumbers
-        }`;
+      const verseId = `v-${
+        Array.isArray(verseNumbers)
+          ? verseNumbers[verseNumbers.length - 1]
+          : verseNumbers
+      }`;
 
       if (scroll)
         document.getElementById(verseId)?.scrollIntoView({
@@ -1329,10 +1336,11 @@ function ThePage({
     (verseNumbers) => {
       if (!tab?.id) return;
 
-      const verseId = `v-${typeof verseNumbers === "object"
-        ? verseNumbers[verseNumbers.length - 1]
-        : verseNumbers
-        }`;
+      const verseId = `v-${
+        typeof verseNumbers === "object"
+          ? verseNumbers[verseNumbers.length - 1]
+          : verseNumbers
+      }`;
 
       document.getElementById(verseId).scrollIntoView({
         behavior: "smooth",
@@ -1523,6 +1531,10 @@ function ThePage({
       y: position.y,
     });
   }, [position, dragToolbar]);
+  const removeBibleStack =
+    tags?.settingsConfigs?.presets?.[
+      configBot?.tags?.settingsPreset || thisBot.tags.settingsPreset || "full"
+    ]?.appSettings?.removeBibleStack;
 
   return (
     <div
@@ -1756,17 +1768,19 @@ function ThePage({
               background: "gray",
             }}
           ></div>
-          <div
-            style={{
-              width: "50%",
-              display: "flex",
-              "align-items": "center",
-              "justify-content": "center",
-              position: "relative",
-            }}
-          >
-            <PageToolbar tab={tab} panelId={panelId} />
-          </div>
+          {removeBibleStack ? null : (
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                "align-items": "center",
+                "justify-content": "center",
+                position: "relative",
+              }}
+            >
+              <PageToolbar tab={tab} panelId={panelId} />
+            </div>
+          )}
           <div style={{ height: "160px" }}></div>
 
           {showVerseToolbar &&
@@ -1784,24 +1798,24 @@ function ThePage({
                 style={
                   globalThis.IsMobileNow()
                     ? {
-                      position: "fixed",
-                      left: "50%",
-                      bottom: "20px",
-                      transform: "translateX(-50%)",
-                      zIndex: 10000,
-                      width: "90%",
-                      maxWidth: "420px",
-                      cursor: "default",
-                      userSelect: "none",
-                    }
+                        position: "fixed",
+                        left: "50%",
+                        bottom: "20px",
+                        transform: "translateX(-50%)",
+                        zIndex: 10000,
+                        width: "90%",
+                        maxWidth: "420px",
+                        cursor: "default",
+                        userSelect: "none",
+                      }
                     : {
-                      position: "fixed",
-                      left: toolbarPos.x - 50,
-                      top: toolbarPos.y,
-                      zIndex: 10000,
-                      cursor: dragToolbar ? "grabbing" : "grab",
-                      userSelect: "none",
-                    }
+                        position: "fixed",
+                        left: toolbarPos.x - 50,
+                        top: toolbarPos.y,
+                        zIndex: 10000,
+                        cursor: dragToolbar ? "grabbing" : "grab",
+                        userSelect: "none",
+                      }
                 }
                 className="verse-toolbar"
               >
@@ -1887,8 +1901,9 @@ function ThePage({
               justifyContent: "center",
               // backgroundColor: "#f8f9fa",
             }}
-            className={`pageContainer ${tabEntered ? "tabEntered" : "tabDrop"
-              } ${highlightOnce ? "tabHighlightBg" : ""}`}
+            className={`pageContainer ${
+              tabEntered ? "tabEntered" : "tabDrop"
+            } ${highlightOnce ? "tabHighlightBg" : ""}`}
           >
             <div
               style={{
@@ -2436,8 +2451,9 @@ function Section({
         return (
           <span
             key={i}
-            className={`clickableCursor highlightened ${isActive ? "highlighted-word" : ""
-              }`}
+            className={`clickableCursor highlightened ${
+              isActive ? "highlighted-word" : ""
+            }`}
             style={{ animationDelay: `${i * 0.1}s` }}
             onClick={() => {
               console.log(part.key);
@@ -2604,8 +2620,8 @@ function Section({
                         `${book}-${chapter}-${verse.verseNumber}`
                       ] || commandHighlight.includes(verse.verseNumber)
                         ? highlighted?.[
-                          `${book}-${chapter}-${verse.verseNumber}`
-                        ]?.color
+                            `${book}-${chapter}-${verse.verseNumber}`
+                          ]?.color
                         : "transparent",
                     color:
                       highlighted?.[
@@ -2644,23 +2660,26 @@ function Section({
                       ? "2px dashed var(--spaceSelection)"
                       : "none",
                   }}
-                  className={`sectionText ${verse?.verseNumber.toString() === activeVerse.toString()
-                    ? "highlighted"
-                    : ""
-                    } ${highlighted?.[`${book}-${chapter}-${verse.verseNumber}`]
+                  className={`sectionText ${
+                    verse?.verseNumber.toString() === activeVerse.toString()
+                      ? "highlighted"
+                      : ""
+                  } ${
+                    highlighted?.[`${book}-${chapter}-${verse.verseNumber}`]
                       ? "verse-highlighted"
                       : ""
-                    } ${isClicked ? "verse-clicked" : ""}`}
+                  } ${isClicked ? "verse-clicked" : ""}`}
                 >
                   {!c ? (
                     (() => {
                       const verseContent = renderVerseText(verse);
                       const verseNumberElement = showVerses[activeSpace] ? (
                         <span
-                          className={`sectionTextNumber ${globalThis.studyNotesPresent
-                            ? "clickableCursor"
-                            : ""
-                            }`}
+                          className={`sectionTextNumber ${
+                            globalThis.studyNotesPresent
+                              ? "clickableCursor"
+                              : ""
+                          }`}
                           onClick={() => {
                             if (globalThis.studyNotesPresent) {
                               HighlightStudyNoteSection(verse?.verseNumber);
@@ -2826,8 +2845,9 @@ function Section({
                         style={{
                           display: showVerses[activeSpace] ? "" : "none",
                         }}
-                        className={`sectionTextNumber ${globalThis.studyNotesPresent ? "clickableCursor" : ""
-                          }`}
+                        className={`sectionTextNumber ${
+                          globalThis.studyNotesPresent ? "clickableCursor" : ""
+                        }`}
                         onClick={() => {
                           if (globalThis.studyNotesPresent) {
                             HighlightStudyNoteSection(verse?.verseNumber);
@@ -2910,7 +2930,7 @@ export const ThePageWithPanel = ({ tab }) => {
         initialWidth={gridPortalBot.tags.pixelWidth}
         containerWidth={gridPortalBot.tags.pixelWidth}
         containerHeight={1000}
-        onResize={() => { }}
+        onResize={() => {}}
         otherTab={panalApp}
       >
         <ThePage setPanalApp={setPanalApp} tab={tab} />
@@ -2927,7 +2947,7 @@ export const ThePageWithEditor = ({ tab, setPanalApp, panelId }) => {
   const activeTab = panelId ? globalThis.PanelTabsMap[panelId] || tab : tab;
   console.log("active tab in the page", panelId, activeTab);
   const [enableEditor, setEnableEditor] = useState(false);
-  useEffect(() => { }, [enableEditor]);
+  useEffect(() => {}, [enableEditor]);
   const [data, setData] = useState(() => {
     if (activeTab) {
       return getCachedBibleData(
